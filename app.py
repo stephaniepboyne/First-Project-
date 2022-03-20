0c99d7795a6d5e3c93546414dda81f4fc97c9dd8
stephaniepboyne <stephanie_boyne@yahoo.co.uk>
2022-03-20 19:59:07 +0000

    display artists and new artist finished

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
 from controllers.print_controller import prints_blueprint
+from controllers.artist_controller import artists_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(prints_blueprint)
+app.register_blueprint(artists_blueprint)
 
 @app.route("/")
 def home():
---
+++ controllers/artist_controller.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, Flask, render_template, redirect, request
+
+import repositories.print_repository as print_repository 
+import repositories.artist_repository as artist_repository
+
+from models.artist import Artist
+
+artists_blueprint = Blueprint("artists", __name__)
+
+@artists_blueprint.route("/artists")
+def display_artists():
+    artists = artist_repository.select_all()
+    return render_template("artists/index.html", all_artists = artists)
+
+@artists_blueprint.route("/artists/new", methods=['GET'])
+def new_artist():
+    return render_template("artists/new.html")
+
+@artists_blueprint.route("/artists", methods=['POST'])
+def create_new_artist(): 
+    name = request.form["name"]
+    artist = Artist(name)
+    artist_repository.save(artist)
+    return redirect("/artists")
