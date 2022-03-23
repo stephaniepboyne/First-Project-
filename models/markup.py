5ec634355c1e29eec29b1a63df72fd2564b254bb
stephaniepboyne <stephanie_boyne@yahoo.co.uk>
2022-03-23 17:40:17 +0000

    fixed markup function

--- models/markup.py
+++ models/markup.py
@@ -1,5 +1,13 @@
 def calculate_markup(print):
-    difference = print.price % print.printing_cost
+    difference = print.price - print.printing_cost
     divide = print.printing_cost / difference 
     percent = 100 / divide 
     return percent
+
+
+
+
+
+
+
+    
